b45c756f96552ff32bb68f3cdd9eab678a1f848b
ClayLanders <clayrlanders@gmail.com>
2026-06-24 16:39:23 -0400

    added additional endpoints to the API Explorer

--- frontend/js/api-explorer.js
+++ frontend/js/api-explorer.js
@@ -1,25 +1,87 @@
 async function runEndpoint() {
 
+    const method =
+        document.getElementById(
+            "method-select"
+        ).value;
+
     const endpoint =
         document.getElementById(
             "endpoint-select"
         ).value;
 
-    const response =
-        await fetch(
-            `${API_BASE_URL}${endpoint}`
-        );
+    const requestBody =
+        document.getElementById(
+            "request-body"
+        ).value;
 
-    const data =
-        await response.json();
+    const options = {
 
-    document.getElementById(
-        "api-response"
-    ).textContent =
-        JSON.stringify(
-            data,
-            null,
-            2
-        );
+        method: method
+
+    };
+
+    if (
+        method === "POST" ||
+        method === "PATCH"
+    ) {
+
+        options.headers = {
+
+            "Content-Type":
+                "application/json"
+
+        };
+
+        try {
+
+            options.body =
+                requestBody;
+
+        }
+
+        catch {
+
+            document.getElementById(
+                "api-response"
+            ).textContent =
+                "Invalid JSON";
+
+            return;
+
+        }
+
+    }
+
+    try {
+
+        const response =
+            await fetch(
+                `${API_BASE_URL}${endpoint}`,
+                options
+            );
+
+        const data =
+            await response.json();
+
+        document.getElementById(
+            "api-response"
+        ).textContent =
+            JSON.stringify(
+                data,
+                null,
+                2
+            );
+
+    }
+
+    catch (error) {
+
+        document.getElementById(
+            "api-response"
+        ).textContent =
+            error.message;
+
+    }
 
 }
